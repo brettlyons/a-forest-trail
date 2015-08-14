84bf6975c6e756a3077b96669a12b7d737f6fb96
Brett Lyons <blyons@students.naropa.edu>
2015-08-14 10:44:05 -0600

    story part 1 complete.

--- js/game.js
+++ js/game.js
@@ -3,11 +3,11 @@
     playerEnergy: 100,
     playerSteps: 0
   };
-
+  //grabs the status div
   var status = document.getElementById("status");
+  //fills in the box on the right side of the page with any key/value pair from
+  //the playerStatus struct-obj.
   for(key in playerStatus) {
     status.innerHTML = status.innerHTML + "<div>" + key + ": " + playerStatus[key] + "</div>";
-
   }
-
 })();
