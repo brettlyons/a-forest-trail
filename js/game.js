1953086009d9710e043ac1c37fca1b7828c9eb56
Brett Lyons <blyons@students.naropa.edu>
2015-08-14 11:47:51 -0600

    Story 2 finished.

--- js/game.js
+++ js/game.js
@@ -1,13 +1,35 @@
 (function() {
   var playerStatus = {
     playerEnergy: 100,
-    playerSteps: 0
+    steps: 0
   };
   //grabs the status div
   var status = document.getElementById("status");
+  var leftDiv = document.getElementById("left");
   //fills in the box on the right side of the page with any key/value pair from
   //the playerStatus struct-obj.
-  for(key in playerStatus) {
-    status.innerHTML = status.innerHTML + "<div>" + key + ": " + playerStatus[key] + "</div>";
-  }
+  //updateStatus resets the status
+  function updateStatus () {
+    status.innerHTML = ''; // replace status.innerHTML, don't add to it;
+    for(key in playerStatus) {
+      status.innerHTML = status.innerHTML + "<div>" + key + ": " + playerStatus[key] + "</div>";
+    }
+  };
+  // this runs updateStatus(); every 17 ms or approx. 59 frames per second
+  setInterval( function () {
+    updateStatus();
+   }, 16); //16.6 ms is 60 fps via dimensional analysis.
+  var walkButton = document.getElementById('walkTrailButton');
+  walkButton.removeAttribute("disabled", "true");
+  walkButton.addEventListener('click',
+      function () { // on click, the walk button ...
+        leftDiv.innerHTML = leftDiv.innerHTML
+          + "You continue down the path."
+          + "<p>";  // ... prints to the left div...
+        walkButton.setAttribute("disabled", "true"); // ...is disabled...
+        playerStatus.steps += 1; // ...updates global state ...
+        setTimeout( function () {
+          walkButton.removeAttribute("disabled");
+        }, 3000) // <-- and is re-enabled after 3 secs.
+      });
 })();
